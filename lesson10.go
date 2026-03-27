9231436106cac16c6ae62a1f0297c75260c6f933
soldieroffortune31 <uzan310596@yahoo.com>
2026-03-27 10:01:43 +0700

    lesson 2026-03-26

--- lesson10.go
+++ lesson10.go
@@ -1,26 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+// import (
+// 	"fmt"
+// 	"sync"
+// 	"time"
+// )
 
-func main() {
+// func main() {
 
-	var x = 0
-	var mutex sync.Mutex
-	for i := 1; i <= 1000; i++ {
-		go func() {
-			for j := 1; j <= 100; j++ {
-				mutex.Lock()
-				x = x + 1
-				mutex.Unlock()
-			}
-		}()
-	}
+// 	var x = 0
+// 	var mutex sync.Mutex
+// 	for i := 1; i <= 1000; i++ {
+// 		go func() {
+// 			for j := 1; j <= 100; j++ {
+// 				mutex.Lock()
+// 				x = x + 1
+// 				mutex.Unlock()
+// 			}
+// 		}()
+// 	}
 
-	time.Sleep(5 * time.Second)
-	fmt.Println("counter = ", x)
+// 	time.Sleep(5 * time.Second)
+// 	fmt.Println("counter = ", x)
 
-}
+// }
